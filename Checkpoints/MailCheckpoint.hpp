54bc8d225275e5455344871563104592b4c921a7
a1091793 <a1091793@localhost>
2017-05-22 05:40:32 +0930

    Removing std::cout in mail checkpoint

--- Checkpoints/MailCheckpoint.hpp
+++ Checkpoints/MailCheckpoint.hpp
@@ -43,7 +43,7 @@ public:
             {
                 std::stringstream command;
                 command << command_start.str() << address << " < /dev/null";
-                std::cout << command.str() << std::endl;
+//                std::cout << command.str() << std::endl;
                 system(command.str().c_str());
 
             }
