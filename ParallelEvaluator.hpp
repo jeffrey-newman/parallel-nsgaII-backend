2c53120fdd90f4b877ca4b098115eaaf25e0c1c4
jeffrey-newman <jeffrey.newman.au@gmail.com>
2018-05-31 09:58:05 +0930

    Fix job accounting for population size in evalAndSAve

--- ParallelEvaluator.hpp
+++ ParallelEvaluator.hpp
@@ -515,7 +515,7 @@ public:
                 {
                     (*population)[s_results.tag()]->setObjectives(std::get<0>(objs_constraints_gen_num_dvs));
                     (*population)[s_results.tag()]->setConstraints(std::get<1>(objs_constraints_gen_num_dvs));
-                    ++results_received;
+                    results_received += 1;
                 }
             }
             // sendJob increments 'indivudal'
@@ -527,7 +527,7 @@ public:
         boost::optional<boost::mpi::status> os;
         do
         {
-            if (results_received == population->size()) break;
+            if (results_received >= population->size()) break;
             os = waitForJobToFinish(logging_file);
             if (os)
             {
@@ -535,7 +535,7 @@ public:
                 if (this->do_log > this->OFF) logging_file << world.rank() << ": " <<  boost::posix_time::second_clock::local_time()  << " received from " << s_results.source() << " individual/job number " << s_results.tag() << " with " << objs_constraints_gen_num_dvs << std::endl;
 
                 //Process received job
-                if (s_results.tag() < population->size())
+                if (s_results.tag() <= population->size())
                 {
                     if ( ((*population)[s_results.tag() - 1]->getRealDVVector() == std::get<3>(objs_constraints_gen_num_dvs))
                         &&  ((*population)[s_results.tag() - 1]->getIntDVVector() == std::get<4>(objs_constraints_gen_num_dvs)) )
@@ -543,8 +543,7 @@ public:
                     {
                         (*population)[s_results.tag()]->setObjectives(std::get<0>(objs_constraints_gen_num_dvs));
                         (*population)[s_results.tag()]->setConstraints(std::get<1>(objs_constraints_gen_num_dvs));
-                        ++results_received;
-
+                        results_received += 1;
                     }
                 }
             }
